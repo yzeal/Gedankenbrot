2e94d9d3738d27244fa6987ebbaa4d35be1f2be1
yzeal <yzeal@live.de>
2013-12-25 22:30:15 +0100

    Tutorial-Kamera ohne Kollision

---
+++ Assets/Scripts/CharacterControllerLogic.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerLogic : MonoBehaviour {
+	
+	#region Variables (private)
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float directionDampTime = .25f;
+	[SerializeField]
+	private float directionSpeed = 3.0f;
+	[SerializeField]
+	private float rotationDegreesPerSecond = 120.0f;
+	[SerializeField]
+	private ThirdPersonCamera gamecam;
+	
+	private float speed = 0.0f;
+	private float direction = 0.0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+	private AnimatorStateInfo stateInfo;
+	
+	//Hashes
+	private int m_LocomotionId = 0;
+	
+	#endregion
+	
+	#region Properties (public)
+	
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		
+		if(animator.layerCount >= 2){
+			animator.SetLayerWeight(1, 1);
+		}
+		
+		m_LocomotionId = Animator.StringToHash("Base Layer.Locomotion");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(animator){
+			stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+			
+			horizontal = Input.GetAxis("Horizontal");
+			vertical = Input.GetAxis ("Vertical");
+			
+			StickToWorldspace(this.transform, gamecam.transform, ref direction, ref speed);
+			
+			animator.SetFloat("Speed", speed);
+			animator.SetFloat("Direction", direction, directionDampTime, Time.deltaTime);
+		}
+	
+	}
+	
+	void FixedUpdate(){
+		//Rotate character model if stick is tilted right or left, but only if character is moving in that direction
+		if(IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0))){
+			
+			Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreesPerSecond * (horizontal < 0f ? -1f : 1f), 0f), Mathf.Abs (horizontal));
+			Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+			this.transform.rotation = (this.transform.rotation * deltaRotation);
+		}
+		
+	}
+	
+	#region Methods
+	
+	public void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut){
+		
+		Vector3 rootDirection = root.forward;
+		
+		Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+		
+		speedOut = stickDirection.sqrMagnitude;
+		
+		//Get camera rotation:
+		Vector3 cameraDirection = camera.forward;
+		cameraDirection.y = 0.0f;
+		Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
+	
+		//convert joystick input to worldspace coordinates
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+		
+		float angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+		
+		angleRootToMove /= 180f;
+		
+		directionOut = angleRootToMove * directionSpeed;
+	
+	}
+	
+	public bool IsInLocomotion(){
+		return stateInfo.nameHash == m_LocomotionId;	
+	}
+	
+	#endregion
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	
+	#region Variables (private)
+	
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform followXform;
+	[SerializeField]
+	private Vector3 offset = new Vector3(0f, 1.5f, 0f);
+	
+	private Vector3 lookDir;
+	private Vector3 targetPosition;
+	
+	//Smoothing and damping:
+	private Vector3 velocityCamSmooth = Vector3.zero;
+	[SerializeField]
+	private float camSmoothDampTime = 0.1f;
+	
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		followXform = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void LateUpdate(){
+		
+		Vector3 characterOffset = followXform.position + offset;
+		
+		lookDir = characterOffset - this.transform.position;
+		lookDir.y = 0;
+		lookDir.Normalize();
+		
+		
+		
+		targetPosition = characterOffset + followXform.up * distanceUp - lookDir * distanceAway;
+		Debug.DrawRay(followXform.position, Vector3.up * distanceUp, Color.red);
+		Debug.DrawRay (followXform.position, -1f * followXform.forward * distanceAway, Color.blue);
+		Debug.DrawLine (followXform.position, targetPosition, Color.magenta);
+		
+		
+		
+		smoothPosition(this.transform.position, targetPosition);
+		
+		transform.LookAt(followXform);
+	}
+	
+	#region Methods
+	
+	private void smoothPosition(Vector3 fromPos, Vector3 toPos){
+		
+		this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+	}
+	
+	#endregion
+	
+	
+}
