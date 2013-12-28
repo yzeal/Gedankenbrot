d9171bdaf70550acaac32efe467186e3809d40a9
yzeal <yzeal@live.de>
2013-12-28 21:33:28 +0100

    Kamera ohne Abhänge

---
+++ Assets/Scripts/golemTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class golemTest : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+	
+	private NavMeshAgent agent;
+	private SphereCollider sphere;
+	private bool followCharacter;
+	
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+		sphere = GetComponentInChildren<SphereCollider>();
+		
+		followCharacter = false;
+    }
+	
+    void Update() {
+       
+		if(followCharacter){
+        	agent.SetDestination(player.transform.position);
+		}else{
+			agent.SetDestination(transform.position);	
+		}
+		//agent.Move();
+            
+    }
+	
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			followCharacter = true;
+			Debug.Log("triggered");
+		}
+	}
+	
+	void OnTriggerExit(Collider other){
+		
+	}
+	
+}
